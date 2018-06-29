53f403d99d9a66ad075a7c2f8cf5740d81ec1988
Aaron Power <theaaronepower@gmail.com>
2018-06-29 11:17:36 +0100

    Moved upload_photo to an example

---
+++ examples/upload_photo.rs
@@ -0,0 +1,12 @@
+mod register;
+
+use std::error;
+
+fn main() -> Result<(), Box<error::Error>> {
+    let mastodon = register::get_mastodon_data()?;
+    let input = register::read_line("Enter the path to the photo you'd like to post: ")?;
+
+    mastodon.media(input.into())?;
+
+    Ok(())
+}
